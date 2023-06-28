c3c73159c04337a688d4b4094c2f079f172d37bf
Guada <guadapdls@gmail.com>
2023-06-28 20:11:34 -0300

    style correction

--- src/Components/TaskListContainer/FormTask/Modal/Modal.jsx
+++ src/Components/TaskListContainer/FormTask/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import FormTask from "../FormTask";
 
 const Modal = ({ task, close }) => {
   return (
-    <section className="fixed bottom-0 left-0 top-0 right-0 flex flex-col items-center justify-center backdrop-blur-sm bg-modal container">
+    <section className="fixed bottom-0 left-0 top-0 right-0 flex flex-col items-center justify-center backdrop-blur-sm bg-modal">
       <div className="bg-sky-200 w-96 rounded-xl">
           <div className="flex justify-between px-5 pt-4 pb-2 mb-2 text-lg border-b border-slate-400">
             <h4>Edit task</h4>
